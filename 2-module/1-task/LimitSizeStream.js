aa856d65782e1e4746d9c3d9f3a31238f96a6de6
Pavel Mashchenko <pmashchenko@gmail.com>
2019-08-31 13:09:32 +0300

    add solution for 2-module 1-task and add solution for 2-module 2-task

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit || Infinity;
+    this.size = '';
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.size += chunk;
+    if (Buffer.byteLength(this.size) > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      this.push(chunk);
+      callback();
+    }
   }
 }
 
--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,24 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.encod = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
+    let data = chunk.toString(this.encod);
+    if (this.last) data = this.last + data;
+    const lines = data.split(os.EOL);
+    this.last = lines.pop();
+
+    lines.forEach((line) => this.push(line));
+    callback();
   }
 
   _flush(callback) {
+    if (this.last) this.push(this.last);
+    this.last = '';
+    callback();
   }
 }
 
