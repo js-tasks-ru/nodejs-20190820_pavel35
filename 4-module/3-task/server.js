a69d063cf82965e933fb1af217bfd3296189e6ac
Pavel Mashchenko <pmashchenko@gmail.com>
2019-09-10 17:58:44 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -9,9 +10,21 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+  }
+
+  if (!fs.existsSync(filepath)) {
+    res.statusCode = 404;
+    res.end('File not found');
+    return;
+  }
+
   switch (req.method) {
     case 'DELETE':
-
+      fs.unlinkSync(filepath);
+      res.end('File deleted');
       break;
 
     default:
